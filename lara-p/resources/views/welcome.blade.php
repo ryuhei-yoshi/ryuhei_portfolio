d51862ef5c248ac6d1650b9cea0ea18f89077a56
ryuhei-yoshi <dogennmosenn@gmail.com>
2021-05-07 22:05:12 +0900

    first

--- lara-p/resources/views/welcome.blade.php
+++ lara-p/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelaaaaaaa
                 </div>
 
                 <div class="links">
